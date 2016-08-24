71fac9daf5c88ee65947bbfba163273eef33ae22
murarikoushik <murarikoushik@yahoo.co.in>
2016-08-24 10:34:31 -0500

    Update Device_2_answer.py
    Added comment

--- Device_2_answer.py
+++ Device_2_answer.py
@@ -13,3 +13,4 @@ from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 device = MonkeyRunner.waitForConnection(5,'84B7N16128001304')
 #device.drag((233,597), (417,605),0.5,50)
 device.touch(650,2075,"DOWN_AND_UP")
+#test
